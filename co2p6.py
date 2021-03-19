6f7189ce714a9d2c1e93160630de5160e3dded07
user <aswanimanojmanas@gmail.com>
2021-03-19 20:54:12 +0530

    commit positive

--- co2p6.py
+++ co2p6.py
@@ -3,4 +3,4 @@ count=0
 for i in range(len(string)):
     if(string[i]!=' '):
         count=count+1
-        print("the number of characyers in the string is",count)
+print("the number of characters in the string is",count)
